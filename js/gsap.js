a4a46bda871299ceb04bae1187f47d12ff41f54a
ssupix <151896598+ssupix@users.noreply.github.com>
2024-06-22 00:04:45 -0700

    hero desktop

---
+++ js/gsap.js
@@ -0,0 +1,37 @@
+// LOGO SPINNER
+
+gsap.registerPlugin(ScrollTrigger);
+  
+    // GSAP timeline for spinning to the right
+  gsap.to(".logo-spinner", {
+    rotation: 360,
+    duration: 10,
+    repeat: -1,
+    ease: "linear"
+  });
+
+  // Function to reverse the spinning direction
+  function reverseSpin() {
+    gsap.to(".logo-spinner", {
+      rotation: -360,
+      duration: 10,
+      repeat: -1,
+      ease: "linear"
+    });
+  }
+
+  // ScrollTrigger to detect scrolling
+  ScrollTrigger.create({
+    onUpdate: (self) => {
+      if (self.direction === 1) { // scrolling down
+        reverseSpin();
+      } else if (self.direction === -1) { // scrolling up
+        gsap.to(".logo-spinner", {
+          rotation: 360,
+          duration: 10,
+          repeat: -1,
+          ease: "linear"
+        });
+      }
+    }
+  });
