b788114b1b8a0bbeb5777117284d7273ebba7f77
ssupix <151896598+ssupix@users.noreply.github.com>
2024-07-18 15:04:08 -0700

    everything

--- js/drag.js
+++ js/drag.js
@@ -1,24 +1,23 @@
-// OCEAN ELEMENTS
-// DRAGGABLE FISH
 
 
-// runs code when all the content is loaded
-document.addEventListener("DOMContentLoaded", function() {
-    // Function to add initial random positions to fish elements
-    function addInitialRandomPositions() {
-        // Select all fish elements
-        const dragElements = document.querySelectorAll(".drag-item");
 
-        // Loop through each fish element and set initial random positions
-        dragElements.forEach(function(item) {
-            item.style.left = Math.random() * (window.innerWidth - item.clientWidth) + "px";
-            item.style.top = Math.random() * (window.innerHeight - item.clientHeight) + "px";
-        });
-    }
+// // runs code when all the content is loaded
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Function to add initial random positions to fish elements
+//     function addInitialRandomPositions() {
+//         // Select all fish elements
+//         const dragElements = document.querySelectorAll(".drag-item");
 
-    // Call the function to add initial random positions to fish elements
-    addInitialRandomPositions();
-});
+//         // Loop through each fish element and set initial random positions
+//         dragElements.forEach(function(item) {
+//             item.style.left = Math.random() * (window.innerWidth - item.clientWidth) + "px";
+//             item.style.top = Math.random() * (window.innerHeight - item.clientHeight) + "px";
+//         });
+//     }
+
+//     // Call the function to add initial random positions to fish elements
+//     addInitialRandomPositions();
+// });
 
 
 // DRAG AND DROP
