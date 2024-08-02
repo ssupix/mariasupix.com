b9ab8bad96ac1b04ed16506c47ce30cf2ee832f2
ssupix <151896598+ssupix@users.noreply.github.com>
2024-08-01 19:05:46 -0700

    nav started

--- js/nav.js
+++ js/nav.js
@@ -29,3 +29,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+// HAMBURGER MENU
+
+
+const hamMenu = document.querySelector(".ham-menu");
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
