22a20cad9d1481a1c8fcf340ea1dcb8be0b23c49
ssupix <151896598+ssupix@users.noreply.github.com>
2024-09-24 14:01:46 -0700

    test compiled

--- index.php
+++ index.php
@@ -127,7 +127,7 @@
                                 <source src="./assets/video/ezt-mockup.ogv" type="video/ogg">
                                 Your browser does not support the video tag.
                             </video>
-                            <img class="video-poster" src="./assets/video/ezt-poster.png" alt="EZT Prototype Mockup" style="display:none;">
+                            <img class="video-poster" src="./assets/video/ezt-poster.png" alt="EZT Prototype Mockup">
                         </div>
                         <h4>ezt - online tea shop</h4>
                         <div class="tags">
